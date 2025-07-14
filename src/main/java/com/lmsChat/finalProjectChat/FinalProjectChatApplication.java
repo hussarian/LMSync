3c23b99f74a431349005861598107afc20763455
youngjae <dudwoeoahs@gmail.com>
2025-07-14 13:11:16 +0900

    07.14-01 프로젝트 생성

---
+++ src/main/java/com/lmsChat/finalProjectChat/FinalProjectChatApplication.java
@@ -0,0 +1,13 @@
+package com.lmsChat.finalProjectChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmsChat/finalProjectChat/FinalProjectChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsChat.finalProjectChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
